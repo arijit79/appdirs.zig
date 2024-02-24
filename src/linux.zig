2839526892de37f12136f3d59a8f6677ba5204af
Arijit Dey <arijit79@protonmail.com>
2024-02-25 00:19:37 +0530

    Implement a extensible way to add OS specific extractors

--- src/linux.zig
+++ src/linux.zig
@@ -1,5 +1,26 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
+const appdirs = @import("main.zig");
+
+pub fn extractOsSpecificPaths(alloc: Allocator) !appdirs.UserDirs {
+    var env_map = try std.process.getEnvMap(alloc);
+    defer env_map.deinit();
+
+    var user_drs = appdirs.UserDirs{
+        .home = try appdirs.copyIfPresent(alloc, env_map.get("HOME")),
+        .audio = null,
+        .desktop = null,
+        .documents = null,
+        .downloads = null,
+        .fomt = null,
+        .pictures = null,
+        .public = null,
+        .templates = null,
+        .videos = null,
+        .alloc = alloc,
+    };
+    return user_drs;
+}
 
 pub fn home_dir(alloc: Allocator) !?[]const u8 {
     var env_map = try std.process.getEnvMap(alloc);
--- src/main.zig
+++ src/main.zig
@@ -7,40 +7,94 @@ const oshandler = switch (builtin.os.tag) {
     else => unreachable,
 };
 
-pub const AppDirs = enum {
-    home,
-    cache,
-    config,
-    local_config,
-    data,
-    data_local,
-    executable,
-    preference,
-    runtime,
-    state,
+pub const AppDirs = struct {
+    const Self = @This();
+    alloc: Allocator,
+    // std_dirs: StandardDirs,
+    user_dirs: UserDirs,
+
+    fn getPath(comptime D: type, dir: D) !?[]const u8 {
+        _ = dir;
+    }
+
+    fn init(alloc: Allocator) !Self {
+        return Self{ .alloc = alloc, .user_dirs = try oshandler.extractOsSpecificPaths(alloc) };
+    }
+
+    fn deinit(self: Self) void {
+        self.user_dirs.deinit();
+    }
 };
 
-pub const UserDirs = enum {
-    home,
-    audio,
-    desktop,
-    documents,
-    downloads,
-    fomt,
-    pictures,
-    public,
-    templates,
-    videos,
+pub const StandardDirs = struct {
+    home: ?[]const u8,
+    cache: ?[]const u8,
+    config: ?[]const u8,
+    local_config: ?[]const u8,
+    data: ?[]const u8,
+    data_local: ?[]const u8,
+    executable: ?[]const u8,
+    preference: ?[]const u8,
+    runtime: ?[]const u8,
+    state: ?[]const u8,
+
+    alloc: Allocator,
+
+    fn deinit(self: @This()) void {
+        comptime var fields = @typeInfo(UserDirs).Struct.fields;
+
+        inline for (fields) |field| {
+            if (field.type == Allocator) {
+                continue;
+            }
+            if (@field(self, field.name)) |f| {
+                self.alloc.free(f);
+            }
+        }
+    }
 };
 
-pub fn getPath(alloc: Allocator, comptime D: type, dir: D) !?[]const u8 {
-    _ = dir;
-    return try oshandler.home_dir(alloc);
+pub const UserDirs = struct {
+    home: ?[]const u8,
+    audio: ?[]const u8,
+    desktop: ?[]const u8,
+    documents: ?[]const u8,
+    downloads: ?[]const u8,
+    fomt: ?[]const u8,
+    pictures: ?[]const u8,
+    public: ?[]const u8,
+    templates: ?[]const u8,
+    videos: ?[]const u8,
+
+    alloc: Allocator,
+
+    fn deinit(self: @This()) void {
+        comptime var fields = @typeInfo(UserDirs).Struct.fields;
+
+        inline for (fields) |field| {
+            if (field.type == Allocator) {
+                continue;
+            }
+            if (@field(self, field.name)) |f| {
+                self.alloc.free(f);
+            }
+        }
+    }
+};
+
+fn copyIfPresent(alloc: Allocator, val: ?[]const u8) !?[]const u8 {
+    if (val) |inner| {
+        return try alloc.dupe(u8, inner);
+    } else {
+        return null;
+    }
 }
 
 test "Test getPath" {
     var allocator = std.testing.allocator;
-    var result = try getPath(allocator, UserDirs, UserDirs.home);
-    defer allocator.free(result.?);
-    std.debug.print("{s}\n", .{result.?});
+    var appdirs = try AppDirs.init(allocator);
+    defer appdirs.deinit();
+    // var result = try getPath(allocator, UserDirs, UserDirs.home);
+    // defer allocator.free(result.?);
+    std.debug.print("{s}\n", .{appdirs.user_dirs.home.?});
 }
